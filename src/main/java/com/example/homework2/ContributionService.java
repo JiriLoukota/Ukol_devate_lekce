5fc206d281a7ce16355687589ee1ec3799bedcbe
Jiří Loukota <jloukota@seznam.cz>
2024-03-22 21:19:15 +0100

    Added methods to save and load data from files.

--- src/main/java/com/example/homework2/ContributionService.java
+++ src/main/java/com/example/homework2/ContributionService.java
@@ -2,6 +2,10 @@ package com.example.homework2;
 
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,11 +14,18 @@ public class ContributionService {
     private final List<Contribution> contributions = new ArrayList<>();
 
     public void addContribution(Contribution contribution) throws Exception{
-        if(!idExists(contribution.getContributionId())) contributions.add(contribution);
+        if(!idExists(contribution.getContributionId())){
+            contributions.add(contribution);
+            try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(""))){
+                bufferedWriter.append(contribution.toSavingFormat());
+            }catch (Exception ignored){}
+
+        }
         else throw new Exception("Contribution with this id already exists.");
     }
 
     public List<Contribution> getVisibleContributions(){
+        loadFile();
         List<Contribution> visibleContributions = new ArrayList<>();
         for(Contribution contribution: contributions){
             if(contribution.isVisible()) visibleContributions.add(contribution);
@@ -22,7 +33,20 @@ public class ContributionService {
         return visibleContributions;
     }
     public List<Contribution> getAllContributions(){
+        loadFile();
         return contributions;
+    }
+
+    private void loadFile(){
+        contributions.clear();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(""))) {
+            while(bufferedReader.readLine() != null){
+                String line = bufferedReader.readLine();
+                String[] parts = line.split("\t");
+                boolean visible = parts[3].equals("true");
+                contributions.add(new Contribution(parts[0], parts[1], Integer.parseInt(parts[2]), visible));
+            }
+        }catch (Exception ignored){}
     }
 
     private boolean idExists(int id){
